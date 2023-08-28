8b5969cb351b6ae92f8972547580902fb48f2547
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2023-08-28 11:38:29 +0200

    Pass number of columns to generate_empty_puzzle.py

--- generate_empty_puzzle.py
+++ generate_empty_puzzle.py
@@ -1,4 +1,14 @@
-size = 6
+import sys
+
+if len(sys.argv) < 2:
+	print("Usage: python " + sys.argv[0] + " <number_of_columns>")
+	sys.exit(1)
+
+size = int(sys.argv[1])
+if size % 2 != 0:
+	print("Argument <number_of_columns> of " + str(size) + " is not a multiple of 2")
+	sys.exit(1)
+
 line = "." * size
 for i in range(size):
 	print(line)
